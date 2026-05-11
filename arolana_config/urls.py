02fdd334c4e232c6c016f4ea5d841012f0c6332b
dinoanthony042-web <dinoanthony042@gmail.com>
2026-05-11 06:45:34 +0100

    images

--- arolana_config/urls.py
+++ arolana_config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 from django.shortcuts import render
 
 # Import views
@@ -118,10 +119,14 @@ urlpatterns = [
 
 
 # =========================================================
-# STATIC / MEDIA (DEV ONLY)
+# STATIC / MEDIA
 # =========================================================
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
 
 # =========================================================
