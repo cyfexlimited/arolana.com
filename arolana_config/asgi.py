d5d98c6cfb8b7884f0900f3d7c4cbc2bc80f85f3
arolana <cyfexlimited8@gmail.com>
2026-05-11 17:29:13 +0100

    Fix asgi.py - simple working version without WebSockets

--- arolana_config/asgi.py
+++ arolana_config/asgi.py
@@ -1,29 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from chat import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arolana_config.settings')
 
-django_asgi_app = get_asgi_application()
-
-# Try to configure WebSocket support
-try:
-    from channels.routing import ProtocolTypeRouter, URLRouter
-    from channels.auth import AuthMiddlewareStack
-    
-    # Import chat routing
-    try:
-        from chat.routing import websocket_urlpatterns
-    except ImportError:
-        websocket_urlpatterns = []
-        print("Warning: chat.routing not available")
-    
-    application = ProtocolTypeRouter({
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        ),
-    })
-    
-except ImportError:
-    application = django_asgi_app
-    print("Warning: channels not available, WebSocket disabled")
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+            path('ws/vendor/<str:vendor_id>/', consumers.VendorChatConsumer.as_asgi()),
+            path('ws/support/', consumers.SupportConsumer.as_asgi()),
+        ])
+    ),
+})
